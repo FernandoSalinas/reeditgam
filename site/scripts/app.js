39181b04b124383c9aa7f5c2a0356a74dc6ed1ea
Fernando <ferisaal@gmail.com>
2015-05-07 12:01:38 -0500

    8 SE ESTILIZA APLICACION Y SE CARGA EL SCRIPT DE

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -1,15 +1,47 @@
 console.log("#Fernando: Cargo app.js");
-var modulo1 = 
-	angular.module("reeditgam",[]);
-modulo1.controller("mainCtrl",[
-	'$scope',
-	function($scope){
-		$scope.test = "Hola Angular";
-		$scope.posts = 
-		["Post 1 Hola",
-		 "Post 2",
-		 "Post 3",
-		 "Post 4",
-		 "Post 5",
-		 "Post 6"];
-	}]);
+ var modulo1=
+ angular.module("reeditgam", []);
+modulo1.controller("mainCtrl",['$scope',
+	function ($scope){
+		$scope.test ="Hola Angular";
+
+		//modelo
+
+	$scope.posts = 
+[{title: "Post 1", upvotes: 5},
+{title: "Post 2", upvotes:15},
+{title: "Post 3", upvotes:7},
+{title: "Post 4", upvotes:9},
+{title: "Post 5", upvotes:3},
+{title: "Post 6", upvotes:5},
+];
+
+//metodo del controlador
+$scope.addPost = function (){
+if(!$scope.title || $scope.title === "")
+{
+alert ("No se permite postear titulos vacios")
+return;
+
+}
+
+$scope.posts.push(
+	{
+		title:$scope.title,
+link: $scope.link,
+		upvotes: 0});
+ //two-way data binding 	
+  //sirve para borrar o limpiar formulario
+$scope.title = "";
+$scope.link = "";
+ };
+
+//metodo que incrementa el voto
+
+//De un post en una unidad
+$scope.incrementUpvotes = function (post){
+post.upvotes += 1;
+
+};
+
+}]);
